5cde3eda18a2baf37e2c1e659605279991e88ea4
Bartek <Barti_x@op.pl>
2021-03-20 15:41:41 +0100

    pipe and ?mark

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { task } from './task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  zmienna = 0;
+
   title = 'apk-web';
+
+  getFooter(): string {
+    if (this.zmienna == 1) { return "Zmienna jest " + this.zmienna }
+    else
+    return "Tekst , który wyświetli się w stopce";
+  }
+
+  add() {
+    this.zmienna++;
+  }
+ 
+  getDate(): Date{
+    return new Date;
+  }
+  
+  tasks: task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false,
+    },
+    {
+      name: 'nauka Angulara',
+      deadline: '2020-03-03',
+      done: false,
+    },
+  ];
+
 }
---
+++ src/app/task.ts
@@ -0,0 +1,5 @@
+export interface task {
+  name: string;
+  deadline: string;
+  done: boolean;
+}
